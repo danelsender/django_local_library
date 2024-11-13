c8e8bc4a670e8ea0d3a29ca260f6006da8e10c4c
danelsender <de296@exeter.ac.uk>
2024-11-13 16:06:11 +0000

    added books list view and book detail view

--- localsite/catalog/views.py
+++ localsite/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -15,13 +16,43 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    
+    num_genres = Genre.objects.count()
+    
+    num_books_with_word = Book.objects.filter(title__icontains='the').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_word': num_books_with_word,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+ 
+class BookListView(generic.ListView):
+   model = Book
+   context_object_name = 'book_list'
+   
+   # queryset = Book.objects.filter(title__icontains='the')[:2] # Get 2 books containing the title 'the'
+   
+   template_name = 'books/book_list.html'
+   
+
+class BookDetailView(generic.DetailView):
+   model = Book
+
+   def book_detail_view(request, primary_key):
+    try:
+        book = Book.objects.get(pk=primary_key)
+    except Book.DoesNotExist:
+        raise Http404('Book does not exist')
+
+    return render(request, 'catalog/book_detail.html', context={'book': book})
+
+   
+   
+    
